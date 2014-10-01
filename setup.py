95ec56984399fa3cf83b5d8c8a8b3b0c60104a51
Ted Lifset <ted@syapse.com>
2014-10-01 13:37:10 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-seleniumfactory',
-    version='0.3.1',
+    version=version,
     description="Python SeleniumFactory",
     long_description="""
 Simple interface factory to create Selenium objects, inspired by
